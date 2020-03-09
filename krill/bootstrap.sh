4acf46d52d5e5b0221333b674073507b44a083a0
Dadepo Aderemi <dadepo@gmail.com>
2020-03-09 15:33:54 +0100

    Updated ROA

--- krill/bootstrap.sh
+++ krill/bootstrap.sh
@@ -12,9 +12,8 @@ krillc repo request --ca ccf-ca > publisher_request.xml
 krillc publishers add -p ccf-ca --rfc8183 publisher_request.xml > repostiroy_response.xml
 krillc repo update embedded --ca ccf-ca
 krillc parents request --ca ccf-ca > myid.xml
-#cargo run --bin krillc children add remote --ca ta --child ccf-ca --ipv4 "10.0.0.0/8" --ipv6 "2001:DB8::/32" --rfc8183 myid.xml > parent-res.xml 
-krillc children add embedded --ca ta --child ccf-ca --ipv4 "10.0.0.0/8" --ipv6 "2001:DB8::/32"
+krillc children add embedded --ca ta --child ccf-ca --ipv4 "100.0.0.0/8"
+krillc children add embedded --ca ta --child ccf-ca --ipv4 "200.0.0.0/8"
 krillc parents add embedded --parent ta 
-#cargo run --bin krillc parents add remote --parent ta --rfc8183 ./parent-res.xml --ca ccf-ca
 krillc roas update --delta ./roas.txt --ca ccf-ca
 
